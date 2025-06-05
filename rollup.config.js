99f5b37a76a44412541bca720ba6d2874fa74cc4
Etienne Maheu <contact@etiennemaheu.com>
2025-06-05 16:10:28 -0400

    Rework build process to support direct zip builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,9 @@ import path from 'path';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
-import copy from 'rollup-plugin-copy';
+import copy from 'rollup-plugin-copy2';
 import {emptyDir} from "rollup-plugin-empty-dir";
+import zip from "rollup-plugin-zip";
 
 /**
  *
@@ -12,20 +13,43 @@ import {emptyDir} from "rollup-plugin-empty-dir";
  */
 export default (args) => {
   console.log(args)
-  const commonPlugins = [
-    replace({
-      preventAssignment: true,
-      values: {
-        '__log_namespace__': JSON.stringify('AuRo ::'),
-        '__log_verbose__': args?.profile === 'verbose' ?? false,
-      }
-    }),
-    nodeResolve(),
-  ];
+  function plugins(build) {
+    return [
+      replace({
+        preventAssignment: true,
+        values: {
+          '__log_namespace__': JSON.stringify('AuRo ::'),
+          '__log_verbose__': args?.profile === 'verbose' ?? false,
+        }
+      }),
+      nodeResolve(),
+      ...(build ? [
+        emptyDir(),
+        copy({
+          assets: [
+            ['src/Icon128-white.png', `Icon128-white.png`],
+            ['dist.content/lib.js', `lib.js`],
+          ],
+        }),
+        chromeExtension(),
+        simpleReloader()
+      ] : [])
+    ];
+  }
 
   return [
     {
+      input: './src/background/library.js',
+      output: {
+        file: 'dist.content/lib.js',
+        format: 'umd',
+        name: 'auro',
+      },
+      plugins: plugins(),
+    },
+    {
       input: `./src/manifest.chrome.json`,
+      cache: true,
       output: {
         dir: 'dist.chrome',
         format: 'es',
@@ -36,58 +60,26 @@ export default (args) => {
       },
       treeshake: false,
       plugins: [
-        emptyDir(),
-        chromeExtension(),
-        simpleReloader(),
-        ...commonPlugins,
-        copy({
-          targets: [
-            { src: 'src/Icon128-white.png', dest: 'dist.chrome' }
-          ]
-        })
+        ...plugins('chrome'),
+        zip(),
       ],
     },
     {
       input: `./src/manifest.firefox.json`,
+      cache: true,
       output: {
         dir: 'dist.firefox',
         format: 'es',
-        globals: [ 'chrome' ]
+        globals: [ 'chrome' ],
+        // HACK: Bypass a bug in chromeExtension() with chunk generation on Windows.
+        // https://github.com/crxjs/chrome-extension-tools/issues/111
+        chunkFileNames: path.join('chunks', '[name]-[hash].js'),
       },
       treeshake: false,
       plugins: [
-        emptyDir(),
-        chromeExtension(),
-        simpleReloader(),
-        ...commonPlugins,
-        copy({
-          targets: [
-            { src: 'src/Icon128-white.png', dest: 'dist.firefox' }
-          ]
-        })
+        ...plugins('firefox'),
+        zip()
       ],
-    },
-    {
-      input: './src/background/library.js',
-      output: {
-        file: 'dist.chrome/lib.js',
-        format: 'umd',
-        name: 'auro',
-      },
-      plugins: [
-        ...commonPlugins,
-      ],
-    },
-    {
-      input: './src/background/library.js',
-      output: {
-        file: 'dist.firefox/lib.js',
-        format: 'umd',
-        name: 'auro',
-      },
-      plugins: [
-        ...commonPlugins,
-      ],
-    },
+    }
   ];
 };
