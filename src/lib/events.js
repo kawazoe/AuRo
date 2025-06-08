78970916969e1d2649d71c81b15a50e54d3b705a
Etienne Maheu <contact@etiennemaheu.com>
2025-06-08 00:08:00 -0400

    Redesign the messaging system to support Firefox quirks
    Firefox doesn't seem to properly support re-entrant messages, that is messages that sends messages, when they also make use of the response API. Instead of correctly waiting for the response and fulfilling the promise, the promises of the entire chain of messages get fulfilled instantly with undefined values.
    
    This solution emulates the response API by opening a separate reply channel for messages.

--- src/lib/events.js
+++ src/lib/events.js
@@ -3,54 +3,104 @@ import 'webextension-polyfill'
 import {log} from "./logging.js";
 import {getFrameDepth} from "./dom.js";
 
-function makeEvent (name, tabs) {
-  function sendRuntime (message) {
-    log('sending message', name, message);
-    return browser.runtime.sendMessage({ name, ...message });
-  }
-  function sendTab (tabId, message) {
-    log('sending message', tabId, name, message);
-    return browser.tabs.sendMessage(tabId, { name, ...message });
-  }
+function toPromise(cb) {
+  return Promise.resolve().then(cb);
+}
 
-  const send = function (...args) {
-    if (tabs) {
-      return sendTab(...args);
-    } else {
-      return sendRuntime(...args);
-    }
-  };
+function makeEvent (kind, tabs) {
+  function sendInner (sender, payload) {
+    const uuid = crypto.randomUUID();
+    const resultPromise = new Promise((resolve, reject) => {
+      log('waiting for response for', kind, uuid);
+      browser.runtime.onMessage.addListener(function responseListener (message) {
+        if (message.uuid !== uuid) {
+          return;
+        }
 
-  send.on = function onMessage (callback) {
-    browser.runtime.onMessage
-      .addListener((message, sender, sendResponse) => {
-        if (message.name === name) {
-          if (tabs && tabs.rootFrame && getFrameDepth() !== 0) {
-            return;
-          }
+        log('got response for', kind, uuid, 'with success', message.success);
+        browser.runtime.onMessage.removeListener(responseListener);
 
-          log('on message', message, sender);
-
-          const result = callback(message, sender);
-
-          function autoForward (response) {
-            if (typeof response === 'undefined') {
-              return sendResponse();
-            }
-
-            log('replying to', message, 'with', response);
-            return sendResponse(response);
-          }
-
-          if (result?.then) {
-            result.then(autoForward);
-            return true;
-          }
-
-          autoForward(result);
+        if (message.success) {
+          resolve(message.payload);
+        } else {
+          reject(new Error(message.error));
         }
       });
+    });
+
+    log('sending message', kind, uuid, payload);
+    sender({ kind, uuid, payload });
+
+    return resultPromise;
   }
+
+  function sendTabs (tabId, message) {
+    return sendInner((p) => browser.tabs.sendMessage(tabId, p), message);
+  }
+  function sendRuntime (message) {
+    return sendInner((p) => browser.runtime.sendMessage(p), message);
+  }
+  const send = tabs ? sendTabs : sendRuntime;
+
+  send.on = function onMessage (callback) {
+    log('listening to', kind, 'on', tabs ?? 'serviceWorker');
+    browser.runtime.onMessage.addListener(function mainListener (message, sender) {
+      if (message.kind !== kind) {
+        return;
+      }
+
+      if (tabs && tabs.rootFrame && getFrameDepth() !== 0) {
+        return;
+      }
+
+      const tabId = sender.tab?.id;
+
+      log('on message', kind, message.uuid, message.payload, sender);
+      const sendResponse = tabId == null
+        ? (m) => {
+          log('replying to message', kind, message.uuid, 'with', m);
+          browser.runtime.sendMessage(m);
+        }
+        : (m) => {
+          log('replying to message', kind, message.uuid, 'with', m);
+          browser.tabs.sendMessage(tabId, m);
+        };
+
+      toPromise(() => callback(message.payload, sender))
+        .then(
+          payload => sendResponse({ kind, uuid: message.uuid, success: true, payload }),
+          error => sendResponse({ kind, uuid: message.uuid, success: false, error })
+        );
+    });
+  }
+
+  // HACK: This is a to-spec implementation of the eventing system. We can't use it because Firefox doesn't support
+  // re-entrant messages properly and resolve the promise returned from sendMessage instantly in those cases. As a
+  // result, we simulate the entire response channel instead. Use this version once Firefox behaves correctly.
+  //
+  // send.on = function onMessage (callback) {
+  //   log('listening to', kind, 'on', tabs);
+  //   browser.runtime.onMessage
+  //     .addListener((message, sender, sendResponse) => {
+  //       if (message.kind !== kind) {
+  //         return;
+  //       }
+  //
+  //       if (tabs && tabs.rootFrame && getFrameDepth() !== 0) {
+  //         return;
+  //       }
+  //
+  //       log('on message', message, sender);
+  //       return toPromise(() => callback(message, sender))
+  //         .then((v) => {
+  //           log('replying to message', message, 'with', v);
+  //           return v;
+  //         }, (e) => {
+  //           error('message', message, 'failed with', e);
+  //           throw e;
+  //         });
+  //     });
+  // }
 
   return send;
 }
